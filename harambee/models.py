41e088fc13016aef08310613005889c7d97a4dd1
Leonardo Donelli <learts92@gmail.com>
2014-11-21 18:03:38 +0100

    Add cap, basic view and controller to create new city.

--- harambee/models.py
+++ harambee/models.py
@@ -6,9 +6,10 @@ db = SQLAlchemy(app)
 class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
+    cap = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return "{}".format(name)
+        return "{}".format(self.name)
 
 class Bug(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- harambee/views.py
+++ harambee/views.py
@@ -1,13 +1,27 @@
 from flask import render_template
+from flask import request
+from flask import redirect, url_for
 from harambee import app
+from harambee.models import City
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/<string:city>")
+@app.route("/city/<string:city>")
 def city(city):
+    city = City.query.filter(City.name == city).first()
     return render_template('city.html', city=city)
+
+@app.route("/city/new", methods=['GET', 'POST'])
+def new_city():
+    if request.method == 'GET':
+        return render_template('new_city.html')
+    else:
+        print(request.form)
+        print(request.args)
+        new_city = City(name=request.form['name'], cap=request.form['cap'])
+        return redirect(url_for('city', city=new_city))
 
 @app.route("/bug")
 def bug():
