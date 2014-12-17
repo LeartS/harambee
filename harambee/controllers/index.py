ea65c3c5b1b34f22a08d357492ff7fe9b4c4d57e
Leonardo Donelli <learts92@gmail.com>
2014-12-17 22:48:54 +0100

    Very basic city search results for those poor souls that can't use typeahead.

--- harambee/controllers/index.py
+++ harambee/controllers/index.py
@@ -1,8 +1,20 @@
 from flask import render_template
+from flask import request
 
 from harambee import app
+from harambee.models import City
 
 
-@app.route("/")
+@app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/search')
+def search():
+    print(repr(request.args['city']))
+    if not request.args['city']:
+        cities = City.query.limit(10).all()
+    else:
+        search_query = '%{}%'.format(request.args['city'])
+        cities = City.query.filter(City.name.ilike(search_query)).all()
+    return render_template('search_results.html', cities=cities)
